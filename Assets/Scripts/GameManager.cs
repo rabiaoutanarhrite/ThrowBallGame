080fa04780f6be239624eb24516ae12957bc3a90
Rabia Outanarhrite <56366894+rabiaoutanarhrite@users.noreply.github.com>
2022-02-05 23:49:26 +0100

    Add New Character as target

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private List<Target> targets = new List<Target>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Target t in targets)
+        {
+            if (!t.isLive)
+            {
+                Debug.Log("Win!");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    public bool isLive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isLive = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            isLive = false;
+            Debug.Log("Done");
+        }
+    }
+}
